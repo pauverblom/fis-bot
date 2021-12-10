9537f3c67e35521a201f06d7441bae1e79c9b2fc
pauverblom <47112988+pauverblom@users.noreply.github.com>
2021-12-10 09:13:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-/*
-const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-const prefix = '!';
-require("dotenv").config();
-
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`)
-})
-
-client.on('interactionCreate', async interaction => {
-  if (!interaction.isCommand()) return;
-
-  if (interaction.commandName === 'si') {
-    await interaction.reply('si?');
-  }
-});
-
-client.login(process.env.TOKEN)
-*/
-
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -26,8 +5,11 @@ client.on('ready', () => {
   console.log(`Logged in...`);
 });
 
-client.on('message', msg => {
-  msg.reply('pong');
+client.on('message', message => {
+  if (message.content === '!Si')
+  {
+     msg.reply('si\nsi\nsi\nsi\nsi\nsi\nsi\nsi\nsi\nsi\n');
+  }
 });
 
 client.login(process.env.TOKEN);
