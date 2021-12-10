d751395bc5de1a3b7a326692685a25452c639013
pauverblom <47112988+pauverblom@users.noreply.github.com>
2021-12-10 08:54:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 const Discord = require('discord.js');
 const client = new Discord.Client({partials: ['MESSAGE', 'CHANNEL', 'REACTION']});
 const prefix = '!';
