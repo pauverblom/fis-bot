0089153b484931450702769357a3b01baf57a7ae
pauverblom <47112988+pauverblom@users.noreply.github.com>
2021-12-10 09:15:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-  if (message.content.toLowercase() == '!si')
+  if (message.content.toLowerCase() == '!si')
   {
      message.reply('si\nsi\nsi\nsi\nsi\nsi\nsi\nsi\nsi\nsi\n');
   }
