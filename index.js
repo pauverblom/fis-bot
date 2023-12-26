41abfb9a3192451cfee167fd86aa3040f03234c3
pauverblom <47112988+pauverblom@users.noreply.github.com>
2023-12-26 11:39:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ client.on('message', message => {
 
     let mensaje = accents.remove(message.content.toLowerCase());
 
-    if (message.author.id === client.user.id) return; //ignorar mensajes propios klom
+    if (message.author.id === client.user.id) return; //ignorar mensajes propios klomp
 
     class examen {
         constructor(nombre, fecha) {
